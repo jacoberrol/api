95a99b93a4c2aa5dae090ff9a007e6f56b2449d4
jakejoh <jakejoh@10.0.0.72>
2019-04-14 17:22:06 -0700

    finished code

--- src/main/java/org/api/Message.java
+++ src/main/java/org/api/Message.java
@@ -1,27 +1,56 @@
 package org.api;
 
 public class Message {
-  private long id;
-  private String content;
 
-  public static final Message EMPTY = new Message(-1L,"");
+	private long id;
+	private String content;
 
-  public Message (long id, String content) {
-    this.id  = id;
-    this.content = content;
-  }
+	public static final Message EMPTY = new Message(-1L,"");
 
-  public String getContent() {
-    return this.content;
-  }
-  public void setContent(String content) {
-    this.content = content;
-  }
+	public Message (long id, String content) {
+		this.id  = id;
+		this.content = content;
+	}
 
-  public long getId() {
-    return this.id;
-  }
-  public void setId(long id) {
-    this.id = id;
-  }
+	public String getContent() {
+		return this.content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public long getId() {
+		return this.id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String toString() {
+		return this.content;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Message other = (Message) obj;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public int hashCode() {
+		return (int)this.id;
+	}
 }
--- src/main/java/org/api/MessageController.java
+++ src/main/java/org/api/MessageController.java
@@ -1,8 +1,11 @@
 package org.api;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +17,14 @@ public class MessageController {
     private final Map<Long,Message> messageMap = new HashMap<Long,Message>();
 
     @RequestMapping(value="/message/:id", method=RequestMethod.GET)
-    public Message getGreeting(long id) {
+    public Message getGreeting(@PathVariable("id") long id) {
       Message rval = this.messageMap.get(id);
       return (rval == null ? Message.EMPTY : rval );
     }
 
     @RequestMapping(value="/message", method=RequestMethod.PUT)
-    public Message putGreeting(String contents) {
-      Message message = new Message(counter.incrementAndGet(),contents);
+    public Message putGreeting(@RequestBody String content) {
+      Message message = new Message(counter.incrementAndGet(),content);
       this.messageMap.put(message.getId(),message);
       return message;
     }
